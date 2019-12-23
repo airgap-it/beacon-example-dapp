0d835808fb48e7b09335f780f00d4b1dab603c3a
Andreas Gassmann <andreas@andreasgassmann.ch>
2019-12-23 14:48:56 +0000

    feat(dapp): add more features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx'
 import { AppComponent } from './app.component'
 import { AppRoutingModule } from './app-routing.module'
 
+import { IonicStorageModule } from '@ionic/storage'
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
   providers: [StatusBar, SplashScreen, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core'
 import { AlertController } from '@ionic/angular'
-import { TezosProtocol } from 'airgap-coin-lib'
+import { TezosFAProtocol } from 'airgap-coin-lib/dist/protocols/tezos/fa/TezosFAProtocol'
+import { PermissionResponse } from '@airgap/beacon-sdk/dist/client/Messages'
+import { Storage } from '@ionic/storage'
 
 declare const window
 
@@ -11,8 +13,11 @@ declare const window
 })
 export class HomePage {
   public connectionStatus: string = 'not connected'
-  public addresses = []
-  constructor(private readonly alertController: AlertController) {
+  public addresses: PermissionResponse = []
+  public contractAddress: string = 'KT1LH2o12xVRwTpJMZ6QJG74Fox8gE9QieFd'
+  public contractBalance: string = ''
+
+  constructor(private readonly alertController: AlertController, private readonly storage: Storage) {
     window.addEventListener('message', async ({ data }) => {
       if (data && data.method && data.data) {
         if (data.method === 'toPage') {
@@ -23,24 +28,44 @@ export class HomePage {
             buttons: ['OK']
           })
 
-          this.addresses = data.data
+          this.addresses = [data.data]
+          this.storage.set('addresses', [data.data])
 
           await alert.present()
         }
       }
     })
+    this.storage.get('addresses').then(res => {
+      if (res) {
+        this.addresses = res
+      }
+    })
   }
 
-  public async initConnection() {}
+  public async initConnection() { }
 
   public async askForPermissions() {
     window.postMessage({ method: 'toExtension', data: 'permission_request' })
   }
 
-  public async sign() {}
+  public async sign() { }
 
   public async getBalanceOfContract() {
-    const contractAddress = ''
-    const protocol = new TezosProtocol()
+    const protocol = new TezosFAProtocol({
+      symbol: 'TZBTC',
+      name: 'Tezos BTC',
+      marketSymbol: 'btc',
+      identifier: 'xtz-btc',
+      contractAddress: this.contractAddress,
+      jsonRPCAPI: "https://tezos-babylonnet-node-1.kubernetes.papers.tech",
+      baseApiUrl: "https://tezos-babylonnet-conseil-1.kubernetes.papers.tech",
+      baseApiKey: 'airgap00391',
+      baseApiNetwork: 'babylonnet'
+    })
+
+    protocol.getBalance(this.addresses[0].address).then(balance => {
+      this.contractBalance = balance
+      console.log('tzbtc balance', balance)
+    })
   }
 }

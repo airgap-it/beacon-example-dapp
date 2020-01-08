f300545c9a1846838751e706777a00ec4440e11f
Andreas Gassmann <andreas@andreasgassmann.ch>
2020-01-08 01:15:28 +0000

    feat(beacon): update beacon sdk

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -4,6 +4,7 @@ import { TezosFAProtocol } from 'airgap-coin-lib/dist/protocols/tezos/fa/TezosFA
 import { PermissionResponse } from '@airgap/beacon-sdk/dist/client/Messages'
 import { Storage } from '@ionic/storage'
 import { DAppClient } from '@airgap/beacon-sdk/dist/client/clients/DappClient'
+import { TezosOperationType } from '@airgap/beacon-sdk/dist/client/operations/OperationTypes'
 
 @Component({
   selector: 'app-home',
@@ -15,18 +16,31 @@ export class HomePage {
   public addresses: PermissionResponse[] = []
   public contractAddress: string = 'KT1LH2o12xVRwTpJMZ6QJG74Fox8gE9QieFd'
   public contractBalance: string = ''
+  public address: string = ''
 
   public client: DAppClient = new DAppClient('DApp')
 
+  public unsignedTransaction: string = ''
+  public broadcastTransaction: string = ''
+
+  public protocol = new TezosFAProtocol({
+    symbol: 'TZBTC',
+    name: 'Tezos BTC',
+    marketSymbol: 'btc',
+    identifier: 'xtz-btc',
+    contractAddress: this.contractAddress,
+    jsonRPCAPI: "https://tezos-babylonnet-node-1.kubernetes.papers.tech",
+    baseApiUrl: "https://tezos-babylonnet-conseil-1.kubernetes.papers.tech",
+    baseApiKey: 'airgap00391',
+    baseApiNetwork: 'babylonnet'
+  })
+
+
   constructor(private readonly alertController: AlertController, private readonly storage: Storage) {
-    window.addEventListener('message', async ({ data }) => {
-      console.log('PAGE', data)
-
-    })
-
     this.storage.get('addresses').then(res => {
       if (res) {
         this.addresses = res
+        this.protocol.getAddressFromPublicKey(this.addresses[0].permissions.pubkey).then(address => this.address = address)
       }
     })
   }
@@ -35,40 +49,83 @@ export class HomePage {
 
   public async askForPermissions() {
     this.client.requestPermissions().then(async response => {
-      console.log('PERMISSION RESULT', response)
-      const alert = await this.alertController.create({
-        header: 'Permissions granted!',
-        message: 'The wallet has granded you permissions to use the address',
-        buttons: ['OK']
-      })
-
-      this.addresses = [response]
-      this.storage.set('addresses', [response])
-
-      await alert.present()
+      this.permissionGrantedAlert(response)
     }).catch(err => { console.log('PERMISSION ERROR', err) })
   }
 
-  public async sign() { }
-
-  public async getBalanceOfContract() {
-    const protocol = new TezosFAProtocol({
-      symbol: 'TZBTC',
-      name: 'Tezos BTC',
-      marketSymbol: 'btc',
-      identifier: 'xtz-btc',
-      contractAddress: this.contractAddress,
-      jsonRPCAPI: "https://tezos-babylonnet-node-1.kubernetes.papers.tech",
-      baseApiUrl: "https://tezos-babylonnet-conseil-1.kubernetes.papers.tech",
-      baseApiKey: 'airgap00391',
-      baseApiNetwork: 'babylonnet'
+  public async permissionGrantedAlert(response: PermissionResponse) {
+    console.log('PERMISSION RESULT', response)
+    const alert = await this.alertController.create({
+      header: 'Permissions granted!',
+      message: 'The wallet has granded you permissions to use the address',
+      buttons: ['OK']
     })
 
-    const address = await protocol.getAddressFromPublicKey(this.addresses[0].permissions.pubkey)
+    this.addresses = [response]
+    this.storage.set('addresses', [response])
+    this.address = await this.protocol.getAddressFromPublicKey(this.addresses[0].permissions.pubkey)
 
-    protocol.getBalance(address).then(balance => {
+    await alert.present()
+  }
+
+  public async getBalanceOfContract() {
+
+    this.protocol.getBalance(this.address).then(balance => {
       this.contractBalance = balance
       console.log('tzbtc balance', balance)
     })
   }
+
+  public async delegate() {
+    this.client.requestOperation({ network: 'mainnet', operationDetails: { kind: TezosOperationType.DELEGATION } }).then(async response => {
+      console.log(response)
+      const alert = await this.alertController.create({
+        header: 'delegate',
+        message: 'Done!',
+        buttons: ['OK']
+      })
+
+      await alert.present()
+    }).catch(err => { console.log('DELEGATE ERROR', err) })
+  }
+
+  public async operationRequest() {
+    this.client.requestOperation({ network: 'mainnet', operationDetails: { kind: TezosOperationType.DELEGATION } }).then(async response => {
+      console.log(response)
+      const alert = await this.alertController.create({
+        header: 'operationRequest',
+        message: 'Done!',
+        buttons: ['OK']
+      })
+
+      await alert.present()
+    }).catch(err => { console.log('OPERATION ERROR', err) })
+  }
+
+  public async sign() {
+    this.client.signPayloads({ payload: [Buffer.from('')], sourceAddress: '' }).then(async response => {
+      console.log(response)
+      const alert = await this.alertController.create({
+        header: 'sign',
+        message: 'Done!',
+        buttons: ['OK']
+      })
+
+      await alert.present()
+    }).catch(err => { console.log('SIGN ERROR', err) })
+  }
+
+  public async broadcast() {
+    this.client.requestBroadcast({ network: 'mainnet', signedTransaction: [Buffer.from(this.broadcastTransaction)] }).then(async response => {
+      console.log(response)
+      const alert = await this.alertController.create({
+        header: 'broadcast',
+        message: 'Done!',
+        buttons: ['OK']
+      })
+
+      await alert.present()
+    }).catch(err => { console.log('BROADCAST ERROR', err) })
+  }
+
 }
